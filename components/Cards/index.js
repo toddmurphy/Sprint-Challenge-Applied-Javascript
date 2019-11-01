0e53484595f8f4d0d9b9a0b514b16fb6fca2dcd9
toddmurphy <toddmurphy93@gmail.com>
2019-11-01 13:23:10 -0300

    got axios request for card data

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -6,50 +6,54 @@
 // This won't be as easy as just iterating over an array though.
 // Create a function that will programmatically create the following DOM component:
 //
-<div class="card">
-  <div class="headline">{Headline of article}</div>
-  <div class="author">
-    <div class="img-container">
-      <img src={url of authors image} />
-    </div>
-    <span>By {authors name}</span>
-  </div>
-</div>
+// <div class="card">
+//   <div class="headline">{Headline of article}</div>
+//   <div class="author">
+//     <div class="img-container">
+//       <img src={url of authors image} />
+//     </div>
+//     <span>By {authors name}</span>
+//   </div>
+// </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
 
 //Main parent to append 'lambdaCard'
 const cards = document.querySelector('.cards-container');
 
+//axios get request
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles').then(response => {
+  const articlesResult = Object.values(response.data);
+  console.log(articlesResult);
+});
+
 //create 'lambdaCard' functional component
-function lambdaCard(){
-//Create elements
-const card = document.createElement('div');//main parent we append all elements -> need to return card
-const headline = document.createElement('div');
-const author = document.createElement('div');
-const imgContainer = document.createElement('div');
-const image = document.createElement('img');
-const authorName = document.createElement('span');
+function lambdaCard() {
+  //Create elements
+  const card = document.createElement('div'); //main parent we append all elements -> need to return card
+  const headline = document.createElement('div');
+  const author = document.createElement('div');
+  const imgContainer = document.createElement('div');
+  const image = document.createElement('img');
+  const authorName = document.createElement('span');
 
-//Set up structure of elements to '.appendChild'
-card.appendChild(headline);
-card.appendChild(author);
-author.appendChild(imgContainer);
-author.appendChild(authorName);
-imgContainer.appendChild(image);
+  //Set up structure of elements to '.appendChild'
+  card.appendChild(headline);
+  card.appendChild(author);
+  author.appendChild(imgContainer);
+  author.appendChild(authorName);
+  imgContainer.appendChild(image);
 
-//Set up class names
-card.classList.add('card');
-headline.classList.add('headline');
-author.classList.add('author');
-imgContainer.classList.add('img-container');
-image.classList.add('img')
-authorName.classList.add('span');
+  //Set up class names
+  card.classList.add('card');
+  headline.classList.add('headline');
+  author.classList.add('author');
+  imgContainer.classList.add('img-container');
+  image.classList.add('img');
+  authorName.classList.add('span');
 
-
-
-//Set up textContent -> to pass data down to from 'lambdaCard' -> from the axios data set
-
-
-return card;
-}//This closes lambdaCard component
+  //Set up textContent -> to pass data down to from 'lambdaCard' -> from the axios data set
+  console.log(card);
+  return card;
+} //This closes lambdaCard component
